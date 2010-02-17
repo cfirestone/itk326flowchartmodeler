dea8c725350c0a5eab2d1744203cbd3276a8113e
jmblakl <jmblakl@953947b8-1b30-11df-8ce3-277986988311>
2010-02-17 00:17:32 +0000

    Top level directory for the project

---
+++ SourceCode/EzDraw/Shapes/SimpleShape.java
@@ -0,0 +1,11 @@
+
+/**
+ * Designed for abstracting the simple shapes
+ * 
+ * @author Justin Blakley & Carl Firestone
+ * @version 1.0.0.0
+ */
+public abstract class SimpleShape extends Shape
+{
+    
+}
