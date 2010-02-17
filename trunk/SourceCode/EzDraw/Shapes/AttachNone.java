78e93403b1f8ce654cd627b9407057c054acd409
jmblakl <jmblakl@953947b8-1b30-11df-8ce3-277986988311>
2010-02-17 00:17:32 +0000

    Top level directory for the project

---
+++ trunk/SourceCode/EzDraw/Shapes/AttachNone.java
@@ -0,0 +1,17 @@
+
+/**
+ * Discribes no attachability
+ * 
+ * @author Justin Blakley & Carl Firestone 
+ * @version 1.0.0.0
+ */
+public class AttachNone implements IAttachable
+{
+    /**
+     * {@inheritDoc}
+     */
+    public double[] calculateAttachmentPoints(double x, double y, double dimX, double dimY)
+    {
+        return new double[0];
+    }
+}
