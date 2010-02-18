d62fc744537cd26f80ae2eb7c3f60c32197a1752
cfirestone <cfirestone@953947b8-1b30-11df-8ce3-277986988311>
2010-02-18 05:35:58 +0000

    Creating Java2D elements

--- SourceCode/EzDraw/Shapes/GUI/GUI.java
+++ SourceCode/EzDraw/Shapes/GUI/GUI.java
@@ -10,12 +10,15 @@ package GUI;
  */
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame {
     private JMenuBar menuBar = new JMenuBar(); // Window menu bar
-    private JMenuItem newItem, openItem, closeItem, saveItem, saveAsItem, printItem, exitItem, lineItem, rectItem, circleItem, textboxItem;
+    private JMenuItem newItem, openItem, closeItem, saveItem, saveAsItem, printItem, exitItem,
+            lineItem, rectItem, circleItem, textboxItem;
 
     public GUI() {
+        this.setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setJMenuBar(menuBar);
         JMenu fileMenu = new JMenu("File");
@@ -36,11 +39,21 @@ public class GUI extends JFrame {
         drawMenu.add(textboxItem = new JMenuItem("Textbox"));
         menuBar.add(fileMenu);
         menuBar.add(drawMenu);
+
+        this.add(new Canvas());
+
     }
 
     public static void main(String[] a) {
         GUI window = new GUI();
         window.setBounds(30, 30, 300, 300); // Size
+        window.setTitle("EzDraw 1.0");
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error setting look and feel");
+        }
         window.setVisible(true);
     }
 }
