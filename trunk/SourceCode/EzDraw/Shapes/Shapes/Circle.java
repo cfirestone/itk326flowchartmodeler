49c4bf301564ca976cb55e9f81533dbe9ea02b80
cfirestone <cfirestone@953947b8-1b30-11df-8ce3-277986988311>
2010-02-22 03:23:46 +0000

    Code Restructure

---
+++ trunk/SourceCode/EzDraw/Shapes/Shapes/Circle.java
@@ -0,0 +1,26 @@
+package Shapes;
+
+/**
+ * Represents a geometric Circle
+ *
+ * @author Justin Blakley & Carl Firestone
+ * @version 1.0.0.0
+ */
+
+public class Circle extends SimpleShape {
+
+    public Circle() {
+        m_AttachmentStrategy = new AttachSides();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void draw() {
+        String eol = System.getProperty("line.separator");
+        System.out.println("I'm a circle being drawn at X: "
+                + getXPosition() + " Y: " + getYPosition()
+                + " with Dimensions X: " + getXDimension() + " Y: "
+                + getYDimension());
+    }
+}
---
+++ trunk/SourceCode/EzDraw/Shapes/Shapes/Rectangle.java
@@ -0,0 +1,27 @@
+package Shapes;
+
+/**
+ * Represents a geometric Rectangle
+ *
+ * @author Justin Blakley & Carl Firestone
+ * @version 1.0.0.0
+ */
+public class Rectangle extends SimpleShape {
+    protected String m_Type;
+
+    public Rectangle() {
+        m_Type = "Rectangle";
+        m_AttachmentStrategy = new AttachSides();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void draw() {
+        String eol = System.getProperty("line.separator");
+        System.out.println("I'm a " + m_Type + " being drawn at X: "
+                + getXPosition() + " Y: " + getYPosition()
+                + " with Dimensions X: " + getXDimension() + " Y: "
+                + getYDimension());
+    }
+}
