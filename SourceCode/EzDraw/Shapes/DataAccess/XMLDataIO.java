ff8e9f8d9aa407b17d522081feee7cf0ca5038e1
jmblakl <jmblakl@953947b8-1b30-11df-8ce3-277986988311>
2010-04-09 05:00:05 +0000

    Implements the FileIO operations.

--- SourceCode/EzDraw/Shapes/DataAccess/XMLDataIO.java
+++ SourceCode/EzDraw/Shapes/DataAccess/XMLDataIO.java
@@ -39,7 +39,7 @@ public class XMLDataIO extends DataIO{
             data.addAll(getLinesFromDocument());
         }
         catch (XPathExpressionException e) {
-            System.out.println("Could not read parse XML Document");
+            System.out.println("Could not parse XML Document");
             e.printStackTrace();  
         }
         return data;
@@ -74,14 +74,21 @@ public class XMLDataIO extends DataIO{
             int x = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("cx").getNodeValue());
             int y = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("cy").getNodeValue());
             int r = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("r").getNodeValue());
+            String fill = nodes.item(i).getAttributes().getNamedItem("fill").getNodeValue();
+            String stroke = nodes.item(i).getAttributes().getNamedItem("stroke").getNodeValue();
+
+            float[] fillArray = convertHexToFloat(fill);
+            float[] strokeArray = convertHexToFloat(stroke);
 
             circle = new Circle(new Point((double)x,(double)y),new Point((double)(x+r),(double)(y+r)));
+
+            circle.setBorderColors(fillArray);
+            circle.setFillColors(strokeArray);
+
             String uri = getNestedDiagram(nodes.item(i));
 
             if(uri != null)
                 circle.setNestedDiagramURL(uri);
-            
-            //String style = nodes.item(i).getAttributes().getNamedItem("style").getNodeValue();
 
             circles.add(circle);
         }
@@ -100,6 +107,11 @@ public class XMLDataIO extends DataIO{
             int y = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("y").getNodeValue());
             int width = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("width").getNodeValue());
             int height = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("height").getNodeValue());
+            String fill = nodes.item(i).getAttributes().getNamedItem("fill").getNodeValue();
+            String stroke = nodes.item(i).getAttributes().getNamedItem("stroke").getNodeValue();
+
+            float[] fillArray = convertHexToFloat(fill);
+            float[] strokeArray = convertHexToFloat(stroke);
 
             if(nodes.item(i).getAttributes().getNamedItem("text") != null)
             {
@@ -110,14 +122,14 @@ public class XMLDataIO extends DataIO{
             else {
                 rect = new Rectangle(new Point((double) x, (double) y), new Point((double) x + width, (double) y + height));
             }
-
+            
+            rect.setBorderColors(fillArray);
+            rect.setFillColors(strokeArray);
             String uri = getNestedDiagram(nodes.item(i));
 
             if(uri != null)
                 rect.setNestedDiagramURL(uri);
-
-            String style = nodes.item(i).getAttributes().getNamedItem("style").getNodeValue();
-
+            
             rects.add(rect);
         }
 
@@ -136,14 +148,21 @@ public class XMLDataIO extends DataIO{
             int sy = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("y1").getNodeValue());
             int ex = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("x2").getNodeValue());
             int ey = Integer.parseInt(nodes.item(i).getAttributes().getNamedItem("x2").getNodeValue());
+            String fill = nodes.item(i).getAttributes().getNamedItem("fill").getNodeValue();
+            String stroke = nodes.item(i).getAttributes().getNamedItem("stroke").getNodeValue();
+
+            float[] fillArray = convertHexToFloat(fill);
+            float[] strokeArray = convertHexToFloat(stroke);
 
             line = new Line(new Point((double)sx,(double)sy), new Point((double)ex,(double)ey));
+
+            line.setBorderColors(fillArray);
+            line.setFillColors(strokeArray);
+
             String uri = getNestedDiagram(nodes.item(i));
 
             if(uri != null)
                 line.setNestedDiagramURL(uri);
-
-            String style = nodes.item(i).getAttributes().getNamedItem("style").getNodeValue();
 
             lines.add(line);
         }
@@ -165,6 +184,45 @@ public class XMLDataIO extends DataIO{
         return (NodeList) result;
     }
 
-    private float[] parseFillStyle(String s){return null;}
-    private float[] parseBorderStyle(String s){return null;}
+    private static float[] convertHexToFloat(String hex)
+    {
+        float[] returnVal = new float[3];
+        if(hex.charAt(0)== '#')
+        {
+            hex = hex.substring(1);
+        }
+        int i = 0;
+        while(hex.length() > 0){
+            int digit = convertToInt(hex.charAt(0),hex.charAt(1));
+            returnVal[i] = (float)digit/255;
+            i++;
+            hex = hex.substring(2);
+        }
+        return returnVal;
+    }
+    private static int convertToInt(char c1, char c2)
+    {
+        int one, two;
+        if(c1 == 'a' || c1 == 'A'){one = 10;}
+        else if(c1 == 'b' || c1 == 'B'){one = 11;}
+        else if(c1 == 'c' || c1 == 'C'){one = 12;}
+        else if(c1 == 'd' || c1 == 'D'){one = 13;}
+        else if(c1 == 'e' || c1 == 'E'){one = 14;}
+        else if(c1 == 'f' || c1 == 'F'){one = 15;}
+        else{one = Integer.parseInt(""+c1);}
+
+        if(c2 == 'a' || c2 == 'A'){two = 10;}
+        else if(c2 == 'b' || c2 == 'B'){two = 11;}
+        else if(c2 == 'c' || c2 == 'C'){two = 12;}
+        else if(c2 == 'd' || c2 == 'D'){two = 13;}
+        else if(c2 == 'e' || c2 == 'E'){two = 14;}
+        else if(c2 == 'f' || c2 == 'F'){two = 15;}
+        else{two = Integer.parseInt(""+c2);}
+
+        return (((one + 1)*(two + 1))-1);
+    }
+
+    public static void main(String args[]){
+        convertHexToFloat("#00fF00");
+    }
 }
