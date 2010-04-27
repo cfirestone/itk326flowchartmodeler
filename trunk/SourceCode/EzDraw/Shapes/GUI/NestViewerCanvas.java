c2acba945d02e6127afc65dcdafb3504bda56c9e
jmblakl <jmblakl@953947b8-1b30-11df-8ce3-277986988311>
2010-04-27 19:28:18 +0000

    Changes reflect issues related the the nested viewer

---
+++ trunk/SourceCode/EzDraw/Shapes/GUI/NestViewerCanvas.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2010. Justin Blakley and Carl Firestone
+ */
+
+package GUI;
+
+import java.awt.event.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: justinblakley
+ * Date: Apr 25, 2010
+ * Time: 10:33:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NestViewerCanvas extends Canvas {
+
+    public void actionPerformed(ActionEvent e) {
+        //do nothing
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void mousePressed(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void mouseExited(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
