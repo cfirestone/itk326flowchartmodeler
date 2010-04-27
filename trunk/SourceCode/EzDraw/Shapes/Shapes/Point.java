840dd14eb316fe5a5706f0d89ba4fd580027abe6
jmblakl <jmblakl@953947b8-1b30-11df-8ce3-277986988311>
2010-04-27 21:35:47 +0000

    Java doc mark up for these files

--- trunk/SourceCode/EzDraw/Shapes/Shapes/Point.java
+++ trunk/SourceCode/EzDraw/Shapes/Shapes/Point.java
@@ -14,27 +14,53 @@ public class Point {
     private double x;
     private double y;
 
+    /**
+     * The constructor for Point
+     * @param x The X representation of the cartesian point
+     * @param y The Y representation of the cartesian point
+     */
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Gets the x val of this point
+     * @return The x value
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * Sets the x val of this point
+     * @param x The x value to set
+     */
     public void setX(double x) {
         this.x = x;
     }
 
+    /**
+     * Gets the y val of this point
+     * @return the y value of this point
+     */
     public double getY() {
         return y;
     }
 
+    /**
+     * Sets the y value of this point
+     * @param y the y value to set
+     */
     public void setY(double y) {
         this.y = y;
     }
 
+    /**
+     * Traditional geometric distance formula SQRT((x2-x1)^2 + (y2-y1)^2)
+     * @param other the other point to measure distance
+     * @return the value representing the distance between the points
+     */
     public double getDistance(Point other) {
         return Math.sqrt(Math.pow((x - other.getX()), 2) + Math.pow((y - other.getY()), 2));
     }
